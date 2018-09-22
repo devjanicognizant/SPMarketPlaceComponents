0bccdb5aa14a09ed4657a53f03f459dbf91a1845
medevjani <medevjani@gmail.com>
2018-09-22 21:35:10 +0530

    added icon based navigation webpart..

---
+++ src/webparts/iconBasedNavigation/components/IIconBasedNavigationProps.ts
@@ -0,0 +1,3 @@
+export interface IIconBasedNavigationProps {
+  description: string;
+}
---
+++ src/webparts/iconBasedNavigation/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IIconBasedNavigationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'IconBasedNavigationWebPartStrings' {
+  const strings: IIconBasedNavigationWebPartStrings;
+  export = strings;
+}
